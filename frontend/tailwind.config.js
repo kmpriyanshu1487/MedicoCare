001c7915879342ef182cb5359891e143ebfe13c1
priyanshu Kumar <kmpriyanshu741@gmail.com>
2024-09-14 12:19:48 +0530

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary':"#87a7d4"
+      }
+    },
+  },
+  plugins: [],
+}
